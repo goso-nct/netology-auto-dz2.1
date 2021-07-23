d253129c1039240fc4133e8cdfeb4b509957a1be
Golubev Vladimir <goso@list.ru>
2021-07-23 15:44:43 +0300

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+AQA-19
+
+# Домашнее задание к занятию «2.1. Тестирование веб-интерфейсов»
+
+[![Build status](https://ci.appveyor.com/api/projects/status/ltpg3et2lnmlkwg4?svg=true)](https://ci.appveyor.com/project/goso-nct/netology-auto-dz2-1)
---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,73 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CallbackTest {
+    private WebDriver driver;
+    private static String driverPath= "";
+
+    static {
+        String OS = System.getProperty("os.name").toLowerCase();
+        boolean IS_WINDOWS = OS.indexOf("win") >= 0;
+        boolean IS_LINUX = OS.indexOf("nux") >= 0;
+        if (IS_WINDOWS)
+            driverPath = "driver/windows/chromedriver.exe";
+        if (IS_LINUX)
+            driverPath = "./driver/linux/chromedriver";
+    }
+
+//    @Test
+//    void checkOsDriverPatch() { System.out.println(driverPath); }
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", driverPath);
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestV1() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Василий");
+        elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("alert-success")).getText();
+        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+    }
+/*
+    @Test
+    void shouldTestV2() {
+        driver.get("http://localhost:9999");
+        WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[data-test-id=submit]")).click();
+        String text = driver.findElement(By.className("alert-success")).getText();
+        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+    }
+ */
+}
