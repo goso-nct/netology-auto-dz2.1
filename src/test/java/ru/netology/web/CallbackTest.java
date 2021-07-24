896a30473d73dc2951d78caa6a53a63144a42c7a
Golubev Vladimir <goso@list.ru>
2021-07-24 11:50:09 +0300

    check error texts

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -16,9 +16,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CallbackTest {
 
-    private WebDriver driver;
-    private static String driverPath= "";
+    static ChromeOptions options = new ChromeOptions();
+    WebDriver driver;
+    WebElement form, element;
 
+    private static String driverPath= "";
     static {
         String OS = System.getProperty("os.name").toLowerCase();
         boolean IS_WINDOWS = OS.indexOf("win") >= 0;
@@ -29,36 +31,30 @@ class CallbackTest {
             driverPath = "driver/linux/chromedriver";
     }
 
-//    @Test
-//    void checkOsDriverPatch() { System.out.println(driverPath); }
-
     @BeforeAll
     static void setUpAll() {
         System.setProperty("webdriver.chrome.driver", driverPath);
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
     }
 
     @BeforeEach
     void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
     void tearDown() {
-        driver.quit();
-        driver = null;
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
-
-    //
-    // task 1
-    //
 
     @Test
     void shouldTestV1() {
-        driver.get("http://localhost:9999");
         List<WebElement> elements = driver.findElements(By.className("input__control"));
         elements.get(0).sendKeys("Василий");
         elements.get(1).sendKeys("+79270000000");
@@ -70,7 +66,6 @@ class CallbackTest {
 
     @Test
     void shouldTestV2() {
-        driver.get("http://localhost:9999");
         WebElement form = driver.findElement(By.xpath("//form"));
         form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
         form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
@@ -80,65 +75,61 @@ class CallbackTest {
         assertTrue(text.contains("Ваша заявка успешно отправлена!"));
     }
 
-    //
-    // task 2
-    //
-
     @Test
     void emptyUser() {
-        driver.get("http://localhost:9999");
-        WebElement form = driver.findElement(By.xpath("//form"));
+        form = driver.findElement(By.xpath("//form"));
         form.findElement(By.tagName("button")).click();
-        WebElement element = form.findElement(By.cssSelector("[data-test-id=name]"));
-        // input input_type_text input_view_default input_size_m input_width_available input_has-label input_invalid input_theme_alfa-on-white
+        element = form.findElement(By.cssSelector("[data-test-id=name]"));
         boolean b = element.getAttribute("class").contains("input_invalid");
         assertTrue(b);
+        element = form.findElement(By.xpath("//*[@id=\"root\"]/div/form/div[1]/span/span/span[3]"));
+        assertTrue(element.getText().equals("Поле обязательно для заполнения"));
     }
 
     @Test
     void emptyPhone() {
-        driver.get("http://localhost:9999");
-        WebElement form = driver.findElement(By.xpath("//form"));
+        form = driver.findElement(By.xpath("//form"));
         form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
         form.findElement(By.tagName("button")).click();
-        WebElement element = form.findElement(By.cssSelector("[data-test-id=phone]"));
-        // input input_type_tel input_view_default input_size_m input_width_available input_has-label input_invalid input_theme_alfa-on-white
+        element = form.findElement(By.cssSelector("[data-test-id=phone]"));
         boolean b = element.getAttribute("class").contains("input_invalid");
         assertTrue(b);
+        element = form.findElement(By.xpath("//*[@id=\"root\"]/div/form/div[2]/span/span/span[3]"));
+        assertTrue(element.getText().equals("Поле обязательно для заполнения"));
     }
 
     @Test
     void invalidUser() {
-        driver.get("http://localhost:9999");
-        WebElement form = driver.findElement(By.xpath("//form"));
+        form = driver.findElement(By.xpath("//form"));
         form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ivanov");
         form.findElement(By.tagName("button")).click();
-        WebElement element = form.findElement(By.cssSelector("[data-test-id=name]"));
+        element = form.findElement(By.cssSelector("[data-test-id=name]"));
         boolean b = element.getAttribute("class").contains("input_invalid");
         assertTrue(b);
+        element = form.findElement(By.xpath("//*[@id=\"root\"]/div/form/div[1]/span/span/span[3]"));
+        assertTrue(element.getText().equals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void invalidPhone() {
-        driver.get("http://localhost:9999");
-        WebElement form = driver.findElement(By.xpath("//form"));
+        form = driver.findElement(By.xpath("//form"));
         form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
         form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+7927");
         form.findElement(By.tagName("button")).click();
-        WebElement element = form.findElement(By.cssSelector("[data-test-id=phone]"));
+        element = form.findElement(By.cssSelector("[data-test-id=phone]"));
         boolean b = element.getAttribute("class").contains("input_invalid");
         assertTrue(b);
+        element = form.findElement(By.xpath("//*[@id=\"root\"]/div/form/div[2]/span/span/span[3]"));
+        assertTrue(element.getText().equals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void checkBoxNotPressed() {
-        driver.get("http://localhost:9999");
-        WebElement form = driver.findElement(By.xpath("//form"));
+        form = driver.findElement(By.xpath("//form"));
         form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
         form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
         form.findElement(By.tagName("button")).click();
-        WebElement element = form.findElement(By.cssSelector("[data-test-id=agreement]"));
-        // checkbox checkbox_size_m checkbox_theme_alfa-on-white input_invalid
+        element = form.findElement(By.cssSelector("[data-test-id=agreement]"));
         boolean b = element.getAttribute("class").contains("input_invalid");
         assertTrue(b);
     }
